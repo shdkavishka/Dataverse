0a34c574c961419e9223377d4cb8b59cd5dbd8be
Deshan Kavishka <99667129+shdkavishka@users.noreply.github.com>
2024-03-29 09:58:54 +0530

    profile re edit

--- dataverse/src/Components/Dashboard/Header.jsx
+++ dataverse/src/Components/Dashboard/Header.jsx
@@ -1,15 +1,17 @@
 // Header.js
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
-import dropdown from '../../assets/drop.png';
-import user from '../../assets/user.png';
-import logo from '../../assets/logo.png';
-import a from '../../assets/a.png';
-import b from '../../assets/b.png';
-import c from '../../assets/c.png';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./Header.css";
+import dropdown from "../../assets/drop.png";
+import user from "../../assets/user.png";
+import logo from "../../assets/logo.png";
+import a from "../../assets/a.png";
+import b from "../../assets/b.png";
+import c from "../../assets/c.png";
 
 const Header = () => {
+  const navigate = useNavigate();
   // State to toggle dropdown visibility
   const [dropdownVisible, setDropdownVisible] = useState(false);
 
@@ -22,33 +24,42 @@ const Header = () => {
     <div className="header">
       <div className="left">
         {/* Dropdown content */}
-        <button onClick={toggleDropdown}><img src={dropdown} alt="dropdown" /></button>
-        <div id="dropdownContent" className={`dropdown-content ${dropdownVisible ? 'show' : ''}`}>
-        <Link to="/UserPage">Edit User </Link>
-        <Link to="/UserPage">View User </Link>
+        <button onClick={toggleDropdown}>
+          <img src={dropdown} alt="dropdown" />
+        </button>
+        <div
+          id="dropdownContent"
+          className={`dropdown-content ${dropdownVisible ? "show" : ""}`}
+        >
+          <Link to="/UserPage">Edit User </Link>
+          <Link to="/UserPage">View User </Link>
         </div>
-        <img src={logo} alt="" style={{ width: '50px', height: '50px' }} />
+        <img src={logo} alt="" style={{ width: "50px", height: "50px" }} />
         <span>DataVerse</span>
         {/* Dropdown button */}
-
       </div>
       <div className="center">
         {/* Search box */}
         <input type="text" placeholder="Search" />
         {/* Buttons */}
-        <button><img src={a} alt="" /></button>
-        <button> <img src={b} alt="" /></button>
-        <button><img src={c} alt="" /></button>
+        <button>
+          <img src={a} alt="" />
+        </button>
+        <button>
+          {" "}
+          <img src={b} alt="" />
+        </button>
+        <button>
+          <img src={c} alt="" />
+        </button>
         {/* Clickable icon */}
-        <Link to="/UserPage">
+        <button onClick={() => navigate("/profile")}>
           <img src={user} alt="User Icon" />
-        </Link>
+        </button>
       </div>
-      <div className="right">
-
-      </div>
+      <div className="right"></div>
     </div>
   );
-}
+};
 
 export default Header;
