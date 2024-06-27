bbeed4bd5e2a97bbddefb4b06c72698a38f884cf
aaishahhamdha <ah20011024@gmail.com>
2024-06-27 06:53:37 +0530

    prof pop-ups

--- dataverse/src/Components/Profile/pop-ups/DeleteAcc.jsx
+++ dataverse/src/Components/Profile/pop-ups/DeleteAcc.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 import './deleteAcc.css';
+import DeleteConf from "./passwordForDeletion"
 
+
+// AH-- pop up to confirm delete acc
 const DeleteAcc = (props) => {
+    const [confirmButton,setConfirmButton]=useState(false)
+
+    
     return props.trigger ? (
         <div className='DeleteAcc'>
             <div>
                 <div>Are you sure you want to delete your Account permanently?</div>
-                <div><button className='confirm' onClick={() => props.setTrigger(false)}>Confirm</button></div>
+                <div><button className='confirm' onClick={() => setConfirmButton(true)}>Confirm</button></div>
                 <div><button className='cancel' onClick={() => props.setTrigger(false)}>Cancel</button></div>
+
+                <DeleteConf trigger={confirmButton} setTrigger={setConfirmButton} /> 
             </div>
         </div>
     ) : null;
