b6f1ee6f8563dcaee9c2f3d34deb8a0c41b9d489
aaishahhamdha <ah20011024@gmail.com>
2024-07-07 20:20:18 +0530

    connect db modif

--- dvBackend/backend/connectDb/views.py
+++ dvBackend/backend/connectDb/views.py
@@ -9,6 +9,7 @@ from .serializers import ConnectedDatabaseSerializer
 from django.contrib.auth.models import User
 from rest_framework.exceptions import PermissionDenied
 from users.models import User
+from rest_framework.exceptions import AuthenticationFailed
 
 # View for creating and listing connected databases
 class ConnectedDatabaseListCreate(ListCreateAPIView):
@@ -38,13 +39,15 @@ class ConnectedDatabaseListCreate(ListCreateAPIView):
             raise PermissionDenied('JWT token not provided')
 
         try:
-            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
-            user_id = payload['id']
+            payload = jwt.decode(token, key='secret', algorithms=['HS256'])
+            user = User.objects.filter(id=payload['id']).first()
+            if not user:
+                raise AuthenticationFailed('User not found!')
+    
         except jwt.ExpiredSignatureError:
             raise PermissionDenied('JWT token expired')
         except jwt.InvalidTokenError:
             raise PermissionDenied('Invalid JWT token')
 
         # Add user ID to serializer data before saving
-        user = User.objects.get(id=user_id)
         serializer.save(owner=user)
